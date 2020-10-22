de80389ae23cb60347e72ad38a8035e482f686b6
Konrad Klimaszewski <graag666@gmail.com>
2020-10-22 16:18:39 +0200

    Another try to fix Catch2.

--- tests/cmake/catch.cmake
+++ tests/cmake/catch.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 ExternalProject_Add(
   catch
   PREFIX ${CMAKE_BINARY_DIR}/catch
-  URL "https://raw.githubusercontent.com/catchorg/Catch2/v2.13.2/single_include/catch.hpp"
+  URL "https://raw.githubusercontent.com/catchorg/Catch2/Catch1.x/single_include/catch.hpp"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
@@ -17,11 +17,11 @@ set(CATCH_MODULE_PATH ${DOWNLOAD_DIR} CACHE INTERNAL "Path to include catch")
 
 # Download module for CTest integration
 if(NOT EXISTS "${CATCH_MODULE_PATH}/Catch.cmake")
-    file(DOWNLOAD "https://raw.githubusercontent.com/catchorg/Catch2/v2.13.2/contrib/Catch.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/catchorg/Catch2/v2.x/contrib/Catch.cmake"
         "${CATCH_MODULE_PATH}/Catch.cmake")
 endif()
 if(NOT EXISTS "${CATCH_MODULE_PATH}/CatchAddTests.cmake")
-    file(DOWNLOAD "https://raw.githubusercontent.com/catchorg/Catch2/v2.13.2/contrib/CatchAddTests.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/catchorg/Catch2/v2.x/contrib/CatchAddTests.cmake"
         "${CATCH_MODULE_PATH}/CatchAddTests.cmake")
 endif()
 
